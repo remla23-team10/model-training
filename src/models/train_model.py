c3c1b3c1e7e9a6f9f67b4f0870b35bc0678c04a6
enriquebarba97 <35747594+enriquebarba97@users.noreply.github.com>
2023-06-05 13:46:52 +0200

    Add robustness test

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,11 +5,12 @@ import os
 import joblib
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
-if __name__ == '__main__':
+
+def train(seed=0):
     # Load data
     corpus = joblib.load('data/processed/corpus.joblib')
     dataset = pd.read_csv('data/external/a1_RestaurantReviews_HistoricDump.tsv',
@@ -20,7 +21,7 @@ if __name__ == '__main__':
     # Train
     X = cv.fit_transform(corpus).toarray()
     y = dataset.iloc[:, -1].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = seed)
 
     classifier = GaussianNB()
     classifier.fit(X_train, y_train)
@@ -29,11 +30,25 @@ if __name__ == '__main__':
     joblib.dump(cv, 'data/processed/BoW_Vectorizer.joblib')
     joblib.dump(classifier, 'models/Classifier_Sentiment_Model.joblib')
 
-    # Save metrics
-    y_pred = classifier.predict(X_test)
-    # print(accuracy_score(y_test, y_pred))
+    return evaluate_model(classifier, X_test, y_test)
+
+def evaluate_model(classifier, X_test, y_test):
+    y_test_prediction = classifier.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_test_prediction)
+    f1 = f1_score(y_test, y_test_prediction)
+    precision = precision_score(y_test, y_test_prediction)
+    recall = recall_score(y_test, y_test_prediction)
+
+    return accuracy, f1, precision, recall
+
+
+
+if __name__ == '__main__':
+    
+    accuracy, f1, precision, recall = train()
 
     if not os.path.exists('models/metrics'):
         os.makedirs('models/metrics')
     with open('models/metrics/Classifier_Sentiment_Model.json', 'w', encoding='utf-8') as file:
-        json.dump({"accuracy": accuracy_score(y_test, y_pred)}, file)
+        json.dump({"accuracy": accuracy, "f1-score": f1, "precision": precision, "recall": recall}, file)
