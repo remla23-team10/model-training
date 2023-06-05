313a7bba581344be5934774db155ecc968c8c2d9
SafEight <Saf@Saf.Saf>
2023-06-05 14:38:11 +0200

    Fix lint

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -11,59 +11,43 @@ from sklearn.naive_bayes import GaussianNB
 
 
 def train(seed=0):
+    """Trains the model and saves it to disk"""
     # Load data
     corpus = joblib.load('data/processed/corpus.joblib')
     dataset = pd.read_csv('data/external/a1_RestaurantReviews_HistoricDump.tsv',
                           delimiter = '\t', quoting = 3,
                           dtype={'Review': str, 'Liked': bool})
-    cv = CountVectorizer(max_features = 1440)
-    return corpus, dataset, cv
-
-def get_split(random_state=42):
-    """Get the train/test split"""
-    corpus, dataset, cv = load_data()
-    X = cv.fit_transform(corpus).toarray()
-    y = dataset.iloc[:, -1].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = seed)
-
-    classifier = GaussianNB()
-    classifier.fit(X_train, y_train)
-    return classifier, X_test, y_test
-
-def accuracy(classifier, X_test, y_test):
-    y_pred = classifier.predict(X_test)
-    return accuracy_score(y_test, y_pred)
-
-if __name__ == '__main__':
-    # Load data
-    corpus, dataset, cv = load_data()
+    count_vectorizer = CountVectorizer(max_features = 1440)
 
     # Train
-    classifier, X_test, y_test = train_data()
+    x_train_array = count_vectorizer.fit_transform(corpus).toarray()
+    y_values = dataset.iloc[:, -1].values
+    x_train, x_test, y_train, y_test = train_test_split(x_train_array, y_values, test_size = 0.20, random_state = seed)
+
+    classifier = GaussianNB()
+    classifier.fit(x_train, y_train)
 
     # Save model
-    joblib.dump(cv, 'data/processed/BoW_Vectorizer.joblib')
+    joblib.dump(count_vectorizer, 'data/processed/BoW_Vectorizer.joblib')
     joblib.dump(classifier, 'models/Classifier_Sentiment_Model.joblib')
 
-    return evaluate_model(classifier, X_test, y_test)
+    return evaluate_model(classifier, x_test, y_test)
 
-def evaluate_model(classifier, X_test, y_test):
-    y_test_prediction = classifier.predict(X_test)
+def evaluate_model(classifier, x_test, y_test):
+    """Evaluates the model and returns the metrics"""
+    y_test_prediction = classifier.predict(x_test)
 
-    accuracy = accuracy_score(y_test, y_test_prediction)
-    f1 = f1_score(y_test, y_test_prediction)
-    precision = precision_score(y_test, y_test_prediction)
-    recall = recall_score(y_test, y_test_prediction)
+    res_accuracy = accuracy_score(y_test, y_test_prediction)
+    res_f1 = f1_score(y_test, y_test_prediction)
+    res_precision = precision_score(y_test, y_test_prediction)
+    res_recall = recall_score(y_test, y_test_prediction)
 
-    return accuracy, f1, precision, recall
-
-
+    return res_accuracy, res_f1, res_precision, res_recall
 
 if __name__ == '__main__':
-    
-    accuracy, f1, precision, recall = train()
+    accuracy, f1_sc, precision, recall = train()
 
     if not os.path.exists('models/metrics'):
         os.makedirs('models/metrics')
     with open('models/metrics/Classifier_Sentiment_Model.json', 'w', encoding='utf-8') as file:
-        json.dump({"accuracy": accuracy, "f1-score": f1, "precision": precision, "recall": recall}, file)
+        json.dump({"accuracy": accuracy, "f1-score": f1_sc, "precision": precision, "recall": recall}, file)
