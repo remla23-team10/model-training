9e46d927fbe94cdc4ce99415b8654aad4113f5bc
SafEight <SafEight>
2023-06-26 19:38:33 +0200

    Update data source url

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -4,7 +4,7 @@ import os
 import urllib.request
 
 if __name__ == '__main__':
-    URL = "https://filesender.surf.nl/download.php?token=615ad465-952d-404a-807c-a407f22b74de&files_ids=13929500"
+    URL = "https://filesender.surf.nl/download.php?token=53ba886c-54c3-43f0-a2ce-6b94bbaf5953&files_ids=14624784"
     FILENAME = "data/external/a1_RestaurantReviews_HistoricDump.tsv"
 
     if not os.path.exists('data/external'):
