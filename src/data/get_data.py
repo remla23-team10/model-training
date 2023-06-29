3afd4f3c1a73ac54c1b1c9ec75c6e3dbd264eb47
SafEight <SafEight>
2023-06-29 19:28:19 +0200

    Revert back to surf

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """This module downloads the data from the SurfDrive"""
 import os
-import gdown
+import urllib.request
 
 if __name__ == '__main__':
-    URL = "https://drive.google.com/file/d/1Uz3o1T1yItRZxAUHTCUoDP3ktglJ0uLO"
+    URL = "https://filesender.surf.nl/download.php?token=53ba886c-54c3-43f0-a2ce-6b94bbaf5953&files_ids=14624784"
     FILENAME = "data/external/a1_RestaurantReviews_HistoricDump.tsv"
 
     if not os.path.exists('data/external'):
         os.makedirs('data/external')
-    gdown.download(URL, FILENAME, quiet=True)
+    downloaded_file, headers = urllib.request.urlretrieve(URL, FILENAME)
