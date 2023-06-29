a3d628e83edd5ea4d322150ba256e295669fa7c5
SafEight <SafEight>
2023-06-29 18:51:51 +0200

    Change get_data to use google drive

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -2,11 +2,14 @@
 """This module downloads the data from the SurfDrive"""
 import os
 import urllib.request
+import dvc
+import gdown
 
 if __name__ == '__main__':
-    URL = "https://filesender.surf.nl/download.php?token=53ba886c-54c3-43f0-a2ce-6b94bbaf5953&files_ids=14624784"
+    URL = "https://drive.google.com/file/d/1Uz3o1T1yItRZxAUHTCUoDP3ktglJ0uLO"
     FILENAME = "data/external/a1_RestaurantReviews_HistoricDump.tsv"
 
     if not os.path.exists('data/external'):
         os.makedirs('data/external')
-    downloaded_file, headers = urllib.request.urlretrieve(URL, FILENAME)
+    # downloaded_file, headers = urllib.request.urlretrieve(URL, FILENAME)
+    gdown.download(URL, FILENAME, quiet=True)
